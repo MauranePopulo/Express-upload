4d33de46c54511d766dc693e7a041a021adcefd3
Maurane Populo <maurane.populo@orange.fr>
2023-11-06 12:00:14 +0100

    upload d'un fichier avec multer et renomer ce fichier puis rendre son nom unique grâce à uuid

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,35 @@
 const express = require("express");
 
 const router = express.Router();
+const fs = require("fs");
+
+// Ajout de multer
+const multer = require("multer");
+
+// Ajout de uuid
+const { v4: uuidv4 } = require("uuid");
+
+// On définit la destination de stockage de nos fichiers
+const upload = multer({ dest: "./public/uploads/" });
+
+// route POST pour recevoir un fichier dont le nom est "avatar"
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  // On récupère le nom du fichier
+  const { originalname } = req.file;
+
+  // On récupère le nom du fichier
+  const { filename } = req.file;
+
+  // On utilise la fonction rename de fs pour renommer le fichier
+  fs.rename(
+    `./public/uploads/${filename}`,
+    `./public/uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 const itemControllers = require("./controllers/itemControllers");
 
